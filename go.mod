12cb920331ccfca88e255c892fe177b86ab8e779
Neil O'Toole <neilotoole@apache.org>
2021-09-14 14:29:36 -0600

    wip: initial mucking around

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/neilotoole/jsoncolor
+
+go 1.16
+
+require (
+	github.com/fatih/color v1.12.0
+	github.com/neilotoole/sq v0.15.3
+	github.com/segmentio/encoding v0.2.20
+	github.com/stretchr/testify v1.7.0
+)
---
+++ jsoncolor.go
@@ -0,0 +1,115 @@
+package jsoncolor
+
+import (
+	"bytes"
+	"strconv"
+
+	"github.com/fatih/color"
+)
+
+// Colors encapsulates colorization of JSON output.
+type Colors struct {
+	Null   Color
+	Bool   Color
+	Number Color
+	String Color
+	Key    Color
+	Bytes  Color
+	Time   Color
+	Punc   Color
+}
+
+// AppendNull appends a colorized "null" to b.
+func (c Colors) AppendNull(b []byte) []byte {
+	b = append(b, c.Null.Prefix...)
+	b = append(b, "null"...)
+	return append(b, c.Null.Suffix...)
+}
+
+// AppendBool appends the colorized bool v to b.
+func (c Colors) AppendBool(b []byte, v bool) []byte {
+	b = append(b, c.Bool.Prefix...)
+
+	if v {
+		b = append(b, "true"...)
+	} else {
+		b = append(b, "false"...)
+	}
+
+	return append(b, c.Bool.Suffix...)
+}
+
+// AppendKey appends the colorized key v to b.
+func (c Colors) AppendKey(b []byte, v []byte) []byte {
+	b = append(b, c.Key.Prefix...)
+	b = append(b, v...)
+	return append(b, c.Key.Suffix...)
+}
+
+// AppendInt64 appends the colorized int64 v to b.
+func (c Colors) AppendInt64(b []byte, v int64) []byte {
+	b = append(b, c.Number.Prefix...)
+	b = strconv.AppendInt(b, v, 10)
+	return append(b, c.Number.Suffix...)
+}
+
+// AppendUint64 appends the colorized uint64 v to b.
+func (c Colors) AppendUint64(b []byte, v uint64) []byte {
+	b = append(b, c.Number.Prefix...)
+	b = strconv.AppendUint(b, v, 10)
+	return append(b, c.Number.Suffix...)
+}
+
+// AppendPunc appends the colorized punctuation mark v to b.
+func (c Colors) AppendPunc(b []byte, v byte) []byte {
+	b = append(b, c.Punc.Prefix...)
+	b = append(b, v)
+	return append(b, c.Punc.Suffix...)
+}
+
+// Color is used to render terminal colors. The Prefix
+// value is written, then the actual value, then the suffix.
+type Color struct {
+	// Prefix is the terminal color code prefix to print before the value (may be empty).
+	Prefix []byte
+
+	// Suffix is the terminal color code suffix to print after the value (may be empty).
+	Suffix []byte
+}
+
+// newColor creates a Color instance from a fatih/color instance.
+func newColor(c *color.Color) Color {
+	// Dirty conversion function ahead: print
+	// a space using c, then grab the bytes printed
+	// before and after the space, and those are the
+	// bytes we need for the prefix and suffix.
+
+	if c == nil {
+		return Color{}
+	}
+
+	// Make a copy because the pkg-level color.NoColor could be false.
+	c2 := *c
+	c2.EnableColor()
+
+	b := []byte(c2.Sprint(" "))
+	i := bytes.IndexByte(b, ' ')
+	if i <= 0 {
+		return Color{}
+	}
+
+	return Color{Prefix: b[:i], Suffix: b[i+1:]}
+}
+
+func NewDefaultColors() Colors {
+	return Colors{
+		Null:   newColor(color.New(color.Faint)),
+		Bool:   newColor(color.New(color.Bold)),
+		Number: newColor(color.New(color.FgCyan)),
+		String: newColor(color.New(color.FgGreen)),
+		Key:    newColor(color.New(color.FgBlue, color.Bold)),
+		Bytes:  newColor(color.New(color.Faint)),
+		Time:   newColor(color.New(color.FgGreen, color.Faint)),
+		Punc:   newColor(color.New(color.Bold)),
+	}
+}
